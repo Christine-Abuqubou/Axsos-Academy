4902246d1d630db948e53f8117f0edd267227f02
christine <ttina2365@gmail.com>
2025-10-01 13:35:01 +0300

    python-stack

---
+++ python-stack/django/djanjo_intro/blog_project/blog_app/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,HttpResponse,redirect
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.http import JsonResponse
+
+def root(request):
+    return redirect('/blogs')
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect('/')
+
+def show(request, number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request, number):
+    return redirect('/blogs')
+
+
+
+def json_response(request):
+    data = {
+        "title": "My Blog Title",
+        "content": "This is the content of the blog."
+    }
+    return JsonResponse(data)
+
+# Create your views here.
