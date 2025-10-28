e6979ba4a04326fcd5847a136bfa7431401ce7cc
christine <ttina2365@gmail.com>
2025-10-28 17:42:35 +0200

    alfred

---
+++ JAVA STACK/AlfredBot/AlfredQuotes.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+
+public class AlfredQuotes {
+
+    public String basicGreeting() {
+        return "Hello, lovely to see you. How are you?";
+    }
+
+    public String guestGreeting(String name) {
+        return String.format("Hello, %s. Lovely to see you.", name);
+    }
+
+    public String dateAnnouncement() {
+        Date date = new Date();
+        return "It is currently " + date.toString();
+    }
+
+    public String respondBeforeAlexis(String conversation) {
+        if (conversation.indexOf("Alexis") >= 0) {
+            return "Right away, sir. She certainly isn't sophisticated enough for that.";
+        } 
+        else if (conversation.indexOf("Alfred") >= 0) {
+            return "At your service. As you wish, naturally.";
+        } 
+        else {
+            return "Right. And with that, I shall retire.";
+        }
+    }
+
+    public String guestGreeting(String name, String dayPeriod) {
+        return String.format("Good %s, %s. Lovely to see you.", dayPeriod, name);
+    }
+
+    public String alfredYell(String phrase) {
+        return phrase.toUpperCase() + "!!! I’m on it, sir!";
+    }
+}
